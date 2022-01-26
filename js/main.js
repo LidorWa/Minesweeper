3ca4aa5ed683b9c2f70c7050a8c467448af7cc8f
Lidor Waldman <95521150+LidorWa@users.noreply.github.com>
2022-01-26 20:56:06 +0200

    test

--- js/main.js
+++ js/main.js
@@ -14,12 +14,12 @@ var gGame = {              // an object indicating the status of the game
     secsPassed: 0         // that key represents the time passed (will start counting on the first cell click)
 };                        // the info will be shown in spans in a modal (or maybe different places)
 var isFirstClick = true;
+// var gCellsCounter = 0;
 
 
 function init() {
     gBoard = buildBoard();
     renderBoard(gBoard);
-    // console.log('init', gBoard);
 }
 
 
@@ -29,6 +29,7 @@ function buildBoard() {
     for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < board[0].length; j++) {
             board[i][j] = { minesAroundCount: 0, isShown: false, isMine: false, isMarked: false };
+            // gCellsCounter++;
         }
     }
     console.log('buildboard', board)
@@ -48,7 +49,6 @@ function renderBoard(board) {
     }
     var elBoard = document.querySelector('.board');
     elBoard.innerHTML = strHTML;
-    // console.log('renderboard', gBoard);
 }
 
 
@@ -69,7 +69,6 @@ function cellClicked(ellCell, cellI, cellJ) {
         // gameOver();
     } else {
         ellCell.classList.add('td_regular');
-        // setMinesNegsCount(cellI,cellJ,gBoard)
         ellCell.innerText = gBoard[cellI][cellJ].minesAroundCount;
     }
 }
@@ -94,7 +93,7 @@ function setMinesNegsCountForCell(cellI, cellJ, board) {                       /
         if (i < 0 || i >= board.length) continue;
 
         for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            // console.log(cellI, cellJ, board[cellI][cellJ]);
+
             if (j < 0 || j >= board[i].length) continue;
 
             if (i === cellI && j === cellJ) continue;
@@ -114,7 +113,7 @@ function emptyCellsArray() {
             array.push({ i: i, j: j });
         }
     }
-    // console.log('empty cells array',gBoard);
+
     return array;
 }
 
